da25125ed7c8ebf8b215cb5c9740dc158118bbe7
unknown <aqibjawwad2607@gmail.com>
2026-06-12 02:17:16 +0600

    test: Fix OrderFactory missing order_number default

--- backend/database/factories/OrderFactory.php
+++ backend/database/factories/OrderFactory.php
@@ -13,7 +13,9 @@ class OrderFactory extends Factory
     public function definition(): array
     {
         return [
+            'order_number'   => 'ORD-' . fake()->unique()->numerify('########'),
             'customer_id'    => User::factory(),
+            'subtotal'       => fake()->randomFloat(2, 50, 5000),
             'total_amount'   => fake()->randomFloat(2, 50, 5000),
             'order_status'   => 'pending',
             'payment_status' => 'pending',
